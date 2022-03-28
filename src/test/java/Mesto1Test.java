e7778e93e19f4168d8c3b76e3c449bbce6fea1f6
viciousmaxx <94640130+viciousmaxx@users.noreply.github.com>
2022-03-28 23:03:33 +0300

    Initial commit

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -6,6 +6,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
 public class Mesto1Test {
 
     String bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTcyZWIzMzMyOTg5YzAwN2Q2M2EyZTAiLCJpYXQiOjE2NDg0OTY0NzcsImV4cCI6MTY0OTEwMTI3N30.GExxFwgGJ2q5ZF6KDEP8y7z9UoosQflzb4BtC8RJFe8";
@@ -13,6 +15,16 @@ public class Mesto1Test {
     @Before
     public void setUp() {
         RestAssured.baseURI = "https://qa-mesto.praktikum-services.ru";
+    }
+
+    @Test
+    @DisplayName("Check user name")
+    @Description("This test is for check current user's name.")
+    public void checkUserName() {
+        given()
+                .auth().oauth2(bearerToken) // Передаём токен для аутентификации
+                .get("/api/users/me") // Делаем GET-запрос
+                .then().assertThat().body("data.name", equalTo("Incorrect Name")); // Проверяем, что имя соответствует ожидаемому
     }
 
     @Test
